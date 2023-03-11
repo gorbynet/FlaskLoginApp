09e4aaa3e753000924acbd0c7ea616f84b6be2fa
Michael Roberts <michael4.roberts@live.uwe.ac.uk>
2023-03-11 17:28:50 +0000

    Initial commit

---
+++ project/__init__.py
@@ -0,0 +1,38 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config['SECRET_KEY'] = b'\x8d?\xe2\xe7\xe3n!\xf9\x99\xc4\x91\xb3\x9a\x7fC\x14' # How to generate this? os.urandom(16)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+
+    db.init_app(app)
+    
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    from .models import User
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        # since the user_id is just the primary key of our user table, use it in the query for the user
+        return User.query.get(int(user_id))
+    
+    from . import models
+    with app.app_context():
+        db.create_all()
+
+    # Need to read up on Flask blueprints...
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint)
+
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    return app
